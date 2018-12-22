19332b0853d8e73f8353a10a02c94dcc5ba660be
maulik kapuria <maulik.j.kapuria@gmail.com>
2018-12-22 15:31:16 -0600

    First commit

---
+++ Overflowtest/Overflowtest.c
@@ -0,0 +1,28 @@
+#include <overflow.h>
+#include <assert.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+
+    a = 0x7fffffff; b = 0x80000001;
+    assert(is_uint_add_overflow(a, b,&c) == false);
+
+    a = 0x7fffffff; b = 0x00000001;
+    assert(is_uint_add_overflow(a, b,&c) == true);
+
+    a = 0x7fffffff; b = 0x00000000;
+    assert(is_uint_add_overflow(a, b,&c) == false);
+
+    a = 0x7fffffff; b = 0x80000000;
+    assert(is_uint_add_overflow(a, b,&c) == false);
+
+    a = 0x7fffffff; b = 0x80000001;
+    assert(is_uint_add_overflow(a, b,&c) == false);
+
+    return(0);
+}
